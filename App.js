d77ea424cc24278fb22e5de84ffec6089c54919b
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-07 12:21:54 +0530

    header

--- App.js
+++ App.js
@@ -16,16 +16,34 @@ import ReactDOM from "react-dom";
  * - Contact
  */
 
+const Header = () => {
+    return (
+          <div className="header">
+                 <div className="logo-container">
+                     <img className="logo" src="https://quickeats.ai/wp-content/uploads/2021/03/QE_Signage-Wordmark-with-Monogram_Solid-Color.png" />
+                 </div>
+                <div className="navbar-item">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+                </div>
+         </div>
+    )
+}
+ 
 
-const AppLayout = () =>{
+const AppLayout = () => {
     return (
         <div className="app">
-
+        <Header/>
         </div>
     )
+}
 
-    }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<AppLayout/>);
