c897dbe61aeacc0cf75fa239335b52d9cd777456
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-08 12:15:41 +0530

    RestaurantCard Component

--- App.js
+++ App.js
@@ -34,11 +34,49 @@ const Header = () => {
     )
 }
  
+const RestaurantCard = () => {
+    return (
+       <div className="res-card">
+         <img className="res-img" alt="res-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0vvulfbahjxjz6k4" />
+         <h3>Meghana Foods</h3>
+         <h4>Biryani, South Indian</h4>
+         <h4>4.6 star</h4>
+         <h4>35-40 MINS</h4>
+       </div> 
+     
+
+    )
+}
+
+
+
+const Body = () =>{
+    return (
+        <div className="body">
+          <div className="search-bar">Search</div>
+          <div className="res-container">
+           <RestaurantCard/>
+           <RestaurantCard/>
+           <RestaurantCard/>
+           <RestaurantCard/>
+           <RestaurantCard/>
+           <RestaurantCard/>
+           <RestaurantCard/>
+           <RestaurantCard/>
+           <RestaurantCard/>
+           <RestaurantCard/>
+          </div>
+       
+        </div>
+
+    )
+}
 
 const AppLayout = () => {
     return (
         <div className="app">
         <Header/>
+        <Body/>
         </div>
     )
 }
